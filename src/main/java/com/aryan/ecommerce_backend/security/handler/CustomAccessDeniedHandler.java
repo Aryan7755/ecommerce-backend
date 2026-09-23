b9e76d0a8b687fe980bc86e118f2cd0e8febea22
Aryan raj <aryanbanthu@gmail.com>
2026-09-23 09:31:59 +0530

    Add custom access denied handler

---
+++ src/main/java/com/aryan/ecommerce_backend/security/handler/CustomAccessDeniedHandler.java
@@ -0,0 +1,25 @@
+package com.aryan.ecommerce_backend.security.handler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import tools.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        response.setContentType("application/json");
+        response.getWriter().write(objectMapper.writeValueAsString(
+                Map.of("status", 403, "message", "Access denied: " + accessDeniedException.getMessage())
+        ));
+    }
+}
