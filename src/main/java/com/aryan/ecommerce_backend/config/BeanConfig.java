505d5ac28fc71cff2342ff7ceba8f54e2bd6b8d0
Aryan raj <aryanbanthu@gmail.com>
2026-09-23 09:39:55 +0530

    Configure authentication manager and password encoder

--- src/main/java/com/aryan/ecommerce_backend/config/BeanConfig.java
+++ src/main/java/com/aryan/ecommerce_backend/config/BeanConfig.java
@@ -2,6 +2,10 @@ package com.aryan.ecommerce_backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -10,5 +14,18 @@ public class BeanConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return  new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            UserDetailsService userDetailsService,
+            PasswordEncoder passwordEncoder) {
+
+        DaoAuthenticationProvider provider =
+                new DaoAuthenticationProvider(userDetailsService);
+
+        provider.setPasswordEncoder(passwordEncoder);
+
+        return new ProviderManager(provider);
     }
 }
